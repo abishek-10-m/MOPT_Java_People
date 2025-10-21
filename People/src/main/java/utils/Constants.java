2e9bb14c051853be349597dc7784b88eee216379
Abishek M <46442328+abishek-10-m@users.noreply.github.com>
2025-10-21 10:49:29 +0000

    fix /Replace Hardcoded Strings with Constants in Constants Class #2

---
+++ People/src/main/java/utils/Constants.java
@@ -0,0 +1,12 @@
+package utils;
+
+public final class Constants {
+    private Constants() {}
+
+    public static final String ARRAYLIST = "ArrayList";
+    public static final String HASHMAP = "HashMap";
+    public static final String FILE = "File";
+    public static final String FILE_SERIALIZATION = "File (Serialization)";
+    public static final String SQL_DATABASE = "SQL - Database";
+    public static final String JPA_DATABASE = "JPA - Database";
+}
